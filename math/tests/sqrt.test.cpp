4935dd938e16282cd3dfd14cd62da59414045ae0
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2020-12-16 17:45:41 -0500

    Added different sqrt impl

--- math/tests/sqrt.test.cpp
+++ math/tests/sqrt.test.cpp
@@ -1,31 +1,46 @@
 #include <gtest/gtest.h>
+#include "math.h"
 #include "test_tools.hpp"
 
 #include "constmath.hpp"
 
 using namespace consteig;
 
-static constexpr double kThresh {0.00000001};
-
 TEST(sqrt_function, sqrt_test)
 {
-    static constexpr double a {sqrt(4.2375)};
-    static constexpr double aAnswer {2.05851888};
-    static_assert(compareFloats(a, aAnswer, kThresh), MSG);
-    ASSERT_TRUE(compareFloats(a, aAnswer, kThresh));
+    static constexpr double a {4.2375};
+    static constexpr double aConsteig {consteig::sqrt(a)};
+    static constexpr double aStd {std::sqrt(a)};
+    static_assert(aConsteig==aStd, MSG);
+    ASSERT_TRUE(aConsteig==aStd);
 
-    static constexpr double b {sqrt(58582.28485)};
-    static constexpr double bAnswer {242.037775667};
-    static_assert(compareFloats(b, bAnswer, kThresh), MSG);
-    ASSERT_TRUE(compareFloats(b, bAnswer, kThresh));
+    static constexpr double b {58582.28485};
+    static constexpr double bConsteig {consteig::sqrt(b)};
+    static constexpr double bStd {std::sqrt(b)};
+    static_assert(bConsteig==bStd, MSG);
+    ASSERT_TRUE(bConsteig==bStd);
 
-    static constexpr int c {sqrt(16)};
+    static constexpr int c {16};
+    static constexpr int cConsteig {consteig::sqrt(c)};
     static constexpr int cAnswer {4};
-    static_assert(c==cAnswer);
-    ASSERT_TRUE(c==cAnswer);
+    static_assert(cConsteig==cAnswer, MSG);
+    ASSERT_TRUE(cConsteig==cAnswer);
 
-    static constexpr int d {sqrt(81)};
-    static constexpr int dAnswer {9};
-    static_assert(d==dAnswer);
-    ASSERT_TRUE(d==dAnswer);
+    static constexpr int d {34225};
+    static constexpr int dConsteig {consteig::sqrt(d)};
+    static constexpr int dAnswer {185};
+    static_assert(dConsteig==dAnswer, MSG);
+    ASSERT_TRUE(dConsteig==dAnswer);
+
+    static constexpr double e {58582.28485};
+    static constexpr double eConsteig {consteig::sqrt(e)};
+    static constexpr double eStd {std::sqrt(e)};
+    static_assert(eConsteig==eStd, MSG);
+    ASSERT_TRUE(eConsteig==eStd);
+
+    //static constexpr long double f {-1.0};
+    //static constexpr long double fConsteig {0};
+    //static constexpr long double fStd {};
+    //static_assert(eConsteig==eStd, MSG);
+    //ASSERT_TRUE(eConsteig==eStd);
 }
--- tools/gtest/test_tools.hpp
+++ tools/gtest/test_tools.hpp
@@ -1,9 +1,34 @@
 #ifndef TEST_TOOLS_HPP
 #define TEST_TOOLS_HPP
 
+#include <limits>
+#include <cfloat>
+
 #include "../consteig.hpp"
 
 #define MSG "Not constexpr or wrong output"
+
+// https://stackoverflow.com/a/32334103/3527182
+template<typename T>
+constexpr bool nearlyEqual(
+  T a,
+  T b,
+  T epsilon = 128 * FLT_EPSILON, T relth = FLT_MIN)
+  // those defaults are arbitrary and could be removed
+{
+  static_assert(consteig::is_float<T>(), "Expects floating point number");
+  assert(std::numeric_limits<T>::epsilon() <= epsilon);
+  assert(epsilon < 1.f);
+
+  if (a == b) return true;
+
+  auto diff = std::abs(a-b);
+  auto norm = std::min((std::abs(a) + std::abs(b)), std::numeric_limits<T>::max());
+  // or even faster: std::min(std::abs(a + b), std::numeric_limits<float>::max());
+  // keeping this commented out until I update figures below
+  return diff < std::max(relth, epsilon * norm);
+}
+
 
 template<size_t R, size_t C>
 static constexpr bool compareFloatMat(
