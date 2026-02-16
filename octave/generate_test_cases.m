75334d1c06e0f75df85b1b37e81dd8797e874ee5
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2026-02-16 23:14:07 +0000

    This now uses octave to generate test cases

--- octave/generate_test_cases.m
+++ octave/generate_test_cases.m
@@ -1,5 +1,13 @@
 % Script to generate C++ test cases for consteig
-% Run this in Octave/Matlab to generate 'generated_test_data.hpp'
+% Run this in Octave/Matlab to generate 'test_dependencies/generated_cases.hpp'
+
+filename = 'test_dependencies/generated_cases.hpp';
+fid = fopen(filename, 'w');
+
+fprintf(fid, '#ifndef GENERATED_CASES_HPP\n');
+fprintf(fid, '#define GENERATED_CASES_HPP\n\n');
+fprintf(fid, '#include "../consteig.hpp"\n\n');
+fprintf(fid, 'namespace consteig {\n\n');
 
 % 1. Symmetric Matrix (Real Eigenvalues)
 S = 5;
@@ -7,130 +15,77 @@ A_sym = rand(S);
 A_sym = A_sym + A_sym'; % Make symmetric
 e_sym = eig(A_sym);
 
-printf("// Symmetric Matrix Test Case\n");
-% Print A_sym
+fprintf(fid, '// Symmetric Matrix Test Case\n');
 [rows, cols] = size(A_sym);
-printf("    static constexpr Matrix<double, %d, %d> mat_sym\n", rows, cols);
-printf("    {{{\n");
+fprintf(fid, 'static constexpr Matrix<double, %d, %d> mat_sym\n', rows, cols);
+fprintf(fid, '{{{\n');
 for i = 1:rows
-    printf("        {");
+    fprintf(fid, '    {');
     for j = 1:cols
         if j > 1
-            printf(", ");
+            fprintf(fid, ', ');
         endif
-        printf("%.16e", A_sym(i,j));
+        fprintf(fid, '%.16e', A_sym(i,j));
     end
     if i < rows
-        printf("},\n");
+        fprintf(fid, '},\n');
     else
-        printf("}\n");
+        fprintf(fid, '}\n');
     endif
 end
-printf("    }}};\n\n");
-
-printf("// Expected Eigenvalues (sorted):\n");
-e_sym = sort(e_sym);
-for i = 1:length(e_sym)
-    printf("// %.16e\n", e_sym(i));
-end
-printf("\n");
+fprintf(fid, '}}};\n\n');
 
 % 2. Non-Symmetric Matrix (Complex Eigenvalues)
 S = 5;
 A_nonsym = rand(S);
 e_nonsym = eig(A_nonsym);
 
-printf("// Non-Symmetric Matrix Test Case\n");
-% Print A_nonsym
+fprintf(fid, '// Non-Symmetric Matrix Test Case\n');
 [rows, cols] = size(A_nonsym);
-printf("    static constexpr Matrix<double, %d, %d> mat_nonsym\n", rows, cols);
-printf("    {{{\n");
+fprintf(fid, 'static constexpr Matrix<double, %d, %d> mat_nonsym\n', rows, cols);
+fprintf(fid, '{{{\n');
 for i = 1:rows
-    printf("        {");
+    fprintf(fid, '    {');
     for j = 1:cols
         if j > 1
-            printf(", ");
+            fprintf(fid, ', ');
         endif
-        printf("%.16e", A_nonsym(i,j));
+        fprintf(fid, '%.16e', A_nonsym(i,j));
     end
     if i < rows
-        printf("},\n");
+        fprintf(fid, '},\n');
     else
-        printf("}\n");
+        fprintf(fid, '}\n');
     endif
 end
-printf("    }}};\n\n");
-
-printf("// Expected Eigenvalues (unsorted complex):\n");
-for i = 1:length(e_nonsym)
-    printf("// %.16e + %.16ei\n", real(e_nonsym(i)), imag(e_nonsym(i)));
-end
-printf("\n");
+fprintf(fid, '}}};\n\n');
 
 % 3. QR Decomposition Test Case
 S = 4;
 A_qr = rand(S);
 [Q, R] = qr(A_qr);
 
-printf("// QR Decomposition Test Case\n");
-% Print A_qr
+fprintf(fid, '// QR Decomposition Test Case\n');
 [rows, cols] = size(A_qr);
-printf("    static constexpr Matrix<double, %d, %d> mat_qr\n", rows, cols);
-printf("    {{{\n");
+fprintf(fid, 'static constexpr Matrix<double, %d, %d> mat_qr\n', rows, cols);
+fprintf(fid, '{{{\n');
 for i = 1:rows
-    printf("        {");
+    fprintf(fid, '    {');
     for j = 1:cols
         if j > 1
-            printf(", ");
+            fprintf(fid, ', ');
         endif
-        printf("%.16e", A_qr(i,j));
+        fprintf(fid, '%.16e', A_qr(i,j));
     end
     if i < rows
-        printf("},\n");
+        fprintf(fid, '},\n');
     else
-        printf("}\n");
+        fprintf(fid, '}\n');
     endif
 end
-printf("    }}};\n\n");
+fprintf(fid, '}}};\n\n');
 
-printf("// Reference Q (Householder from Octave):\n");
-% Print Q
-[rows, cols] = size(Q);
-printf("    static constexpr Matrix<double, %d, %d> ref_q\n", rows, cols);
-printf("    {{{\n");
-for i = 1:rows
-    printf("        {");
-    for j = 1:cols
-        if j > 1
-            printf(", ");
-        endif
-        printf("%.16e", Q(i,j));
-    end
-    if i < rows
-        printf("},\n");
-    else
-        printf("}\n");
-    endif
-end
-printf("    }}};\n\n");
+fprintf(fid, '} // namespace consteig\n');
+fprintf(fid, '#endif\n');
 
-printf("// Reference R:\n");
-% Print R
-[rows, cols] = size(R);
-printf("    static constexpr Matrix<double, %d, %d> ref_r\n", rows, cols);
-printf("    {{{\n");
-for i = 1:rows
-    printf("        {");
-    for j = 1:cols
-        if j > 1
-            printf(", ");
-        endif
-        printf("%.16e", R(i,j));
-    end
-    if i < rows
-        printf("},\n");
-    else
-        printf("}\n");
-    endif
-end
-printf("    }}};\n\n");
+fclose(fid);
