e53e29e9eaef0517e40f1c13f9a5f4ab3cd4b670
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2026-02-16 22:18:51 +0000

    add complex number support

---
+++ math/complex.hpp
@@ -0,0 +1,78 @@
+#ifndef COMPLEX_HPP
+#define COMPLEX_HPP
+
+#include "functions/abs.hpp"
+#include "functions/utilities.hpp"
+
+namespace consteig
+{
+
+template<typename T>
+struct Complex
+{
+    T real;
+    T imag;
+
+    constexpr Complex(T r = 0, T i = 0) : real(r), imag(i) {}
+
+    constexpr Complex operator+(const Complex& other) const
+    {
+        return {real + other.real, imag + other.imag};
+    }
+
+    constexpr Complex operator-(const Complex& other) const
+    {
+        return {real - other.real, imag - other.imag};
+    }
+
+    constexpr Complex operator*(const Complex& other) const
+    {
+        return {real * other.real - imag * other.imag,
+                real * other.imag + imag * other.real};
+    }
+
+    constexpr Complex operator/(const Complex& other) const
+    {
+        T denom = other.real * other.real + other.imag * other.imag;
+        return {(real * other.real + imag * other.imag) / denom,
+                (imag * other.real - real * other.imag) / denom};
+    }
+
+    constexpr bool operator==(const Complex& other) const
+    {
+        return real == other.real && imag == other.imag;
+    }
+
+    constexpr bool operator!=(const Complex& other) const
+    {
+        return !(*this == other);
+    }
+};
+
+template<typename T>
+constexpr Complex<T> operator*(const T& scalar, const Complex<T>& c)
+{
+    return {scalar * c.real, scalar * c.imag};
+}
+
+template<typename T>
+constexpr Complex<T> operator*(const Complex<T>& c, const T& scalar)
+{
+    return scalar * c;
+}
+
+template<typename T>
+constexpr T abs(const Complex<T>& c)
+{
+    return consteig::sqrt(c.real * c.real + c.imag * c.imag);
+}
+
+template<typename T>
+constexpr Complex<T> conj(const Complex<T>& c)
+{
+    return {c.real, -c.imag};
+}
+
+} // namespace consteig
+
+#endif
