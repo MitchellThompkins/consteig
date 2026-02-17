dcb9547e9d2123cba4e1c8d4d625fb268fcec07e
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2026-02-17 22:06:07 +0000

    with optimizations and improvements, see if this symmetric matrix passes now.

--- eigen/tests/generated_sym_33.test.cpp
+++ eigen/tests/generated_sym_33.test.cpp
@@ -2,6 +2,6 @@
 
 TEST(generated_tests, sym_33)
 {
-    // static_assert(check_single_symmetric<33>(), "Sym 33 failed");
+    static_assert(check_single_symmetric<33>(), "Sym 33 failed");
     SUCCEED();
 }
