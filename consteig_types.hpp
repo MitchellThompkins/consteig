c347fe01d579eb92eafd818d86fd88f95134311c
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2026-02-17 00:04:22 +0000

    need to check in these types

---
+++ consteig_types.hpp
@@ -0,0 +1,10 @@
+#ifndef CONSTEIG_TYPES_HPP
+#define CONSTEIG_TYPES_HPP
+
+namespace consteig
+{
+    // Define Size type to avoid dependency on <cstddef>
+    using Size = decltype(sizeof(0));
+}
+
+#endif
