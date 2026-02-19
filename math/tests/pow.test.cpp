766c98a461468ca5300d99295392dfe838678a20
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2026-02-19 07:57:51 +0000

    ran make format

--- math/tests/pow.test.cpp
+++ math/tests/pow.test.cpp
@@ -1,31 +1,30 @@
 #include <gtest/gtest.h>
-#include "test_tools.hpp"
+
 #include "constmath.hpp"
+#include "test_tools.hpp"
 
 using namespace consteig;
 
-static constexpr double kThresh {1e-9};
+static constexpr double kThresh{1e-9};
 
-TEST(power_function, pow_basic)
-{
-    static constexpr double a {consteig::pow(5.3, 6)};
-    static constexpr double aAnswer {22164.361129};
+TEST(power_function, pow_basic) {
+    static constexpr double a{consteig::pow(5.3, 6)};
+    static constexpr double aAnswer{22164.361129};
     static_assert(compareFloats(a, aAnswer, kThresh), MSG);
     ASSERT_TRUE(compareFloats(a, aAnswer, kThresh));
 
-    static constexpr double b {consteig::pow(2.0, 3)};
-    static constexpr double bAnswer {8.0};
+    static constexpr double b{consteig::pow(2.0, 3)};
+    static constexpr double bAnswer{8.0};
     static_assert(compareFloats(b, bAnswer, kThresh), MSG);
     ASSERT_EQ(b, bAnswer);
 
-    static constexpr double c {consteig::pow(3.0, 2)};
-    static constexpr double cAnswer {9.0};
+    static constexpr double c{consteig::pow(3.0, 2)};
+    static constexpr double cAnswer{9.0};
     static_assert(compareFloats(c, cAnswer, kThresh), MSG);
     ASSERT_EQ(c, cAnswer);
 }
 
-TEST(power_function, pow_zero_base)
-{
+TEST(power_function, pow_zero_base) {
     static constexpr double val1 = consteig::pow(0.0, 1);
     static_assert(val1 == 0.0, MSG);
     ASSERT_EQ(val1, 0.0);
@@ -35,8 +34,7 @@ TEST(power_function, pow_zero_base)
     ASSERT_EQ(val2, 0.0);
 }
 
-TEST(power_function, pow_zero_exponent)
-{
+TEST(power_function, pow_zero_exponent) {
     static constexpr double val1 = consteig::pow(5.5, 0);
     static_assert(val1 == 1.0, MSG);
     ASSERT_EQ(val1, 1.0);
@@ -45,13 +43,12 @@ TEST(power_function, pow_zero_exponent)
     static_assert(val2 == 1.0, MSG);
     ASSERT_EQ(val2, 1.0);
 
-    static constexpr double val3 = consteig::pow(0.0, 0); // usually 1
+    static constexpr double val3 = consteig::pow(0.0, 0);  // usually 1
     static_assert(val3 == 1.0, MSG);
     ASSERT_EQ(val3, 1.0);
 }
 
-TEST(power_function, pow_negative_base)
-{
+TEST(power_function, pow_negative_base) {
     // (-2)^2 = 4
     static constexpr double val1 = consteig::pow(-2.0, 2);
     static_assert(val1 == 4.0, MSG);
@@ -63,15 +60,13 @@ TEST(power_function, pow_negative_base)
     ASSERT_EQ(val2, -8.0);
 }
 
-TEST(power_function, pow_one_base)
-{
+TEST(power_function, pow_one_base) {
     static constexpr double val = consteig::pow(1.0, 100);
     static_assert(val == 1.0, MSG);
     ASSERT_EQ(val, 1.0);
 }
 
-TEST(power_function, pow_large)
-{
+TEST(power_function, pow_large) {
     // 2^10 = 1024
     static constexpr int val = consteig::pow(2, 10);
     static_assert(val == 1024, MSG);
--- matrix/decompositions/qr.hpp
+++ matrix/decompositions/qr.hpp
@@ -1,58 +1,54 @@
 #ifndef QR_DECOMP_HPP
 #define QR_DECOMP_HPP
 
-#include "../matrix.hpp"
-#include "../operations.hpp"
 #include "../../math/functions/abs.hpp"
 #include "../../math/functions/sqrt.hpp"
 #include "../../math/functions/utilities.hpp"
+#include "../matrix.hpp"
+#include "../operations.hpp"
 
-namespace consteig
-{
+namespace consteig {
 
-template<typename T, Size S>
-struct QRMatrix
-{
-    Matrix<T,S,S> _q;
-    Matrix<T,S,S> _r;
+template <typename T, Size S>
+struct QRMatrix {
+    Matrix<T, S, S> _q;
+    Matrix<T, S, S> _r;
 };
 
 // Optimized QR for Upper Hessenberg Matrix
-template<typename T, Size R, Size C>
-constexpr QRMatrix<T, R> qr_hessenberg( const Matrix<T,R,C> a )
-{
-    static_assert( R==C, "QR decomposition must be a square matrix");
+template <typename T, Size R, Size C>
+constexpr QRMatrix<T, R> qr_hessenberg(const Matrix<T, R, C> a) {
+    static_assert(R == C, "QR decomposition must be a square matrix");
 
-    Matrix<T,R,R> q = eye<T,R>();
-    Matrix<T,R,R> r = a;
-    
-    for(Size i = 0; i < R - 1; ++i)
-    {
+    Matrix<T, R, R> q = eye<T, R>();
+    Matrix<T, R, R> r = a;
+
+    for (Size i = 0; i < R - 1; ++i) {
         // Use Givens rotation to zero a(i+1, i)
-        T x = r(i,i);
-        T y = r(i+1,i);
-        
+        T x = r(i, i);
+        T y = r(i + 1, i);
+
         if (consteig::abs(y) > static_cast<T>(1e-15)) {
-            T mag = consteig::sqrt(x*x + y*y);
+            T mag = consteig::sqrt(x * x + y * y);
             T c = x / mag;
             T s = y / mag;
-            
+
             // R = G * R
             for (Size j = i; j < R; ++j) {
-                T r_i = r(i,j);
-                T r_ip1 = r(i+1,j);
-                r(i,j) = c * r_i + s * r_ip1;
-                r(i+1,j) = -s * r_i + c * r_ip1;
+                T r_i = r(i, j);
+                T r_ip1 = r(i + 1, j);
+                r(i, j) = c * r_i + s * r_ip1;
+                r(i + 1, j) = -s * r_i + c * r_ip1;
             }
-            
+
             // Q = Q * G^T
             for (Size j = 0; j < R; ++j) {
-                T q_i = q(j,i);
-                T q_ip1 = q(j,i+1);
-                q(j,i) = c * q_i + s * q_ip1;
-                q(j,i+1) = -s * q_i + c * q_ip1;
+                T q_i = q(j, i);
+                T q_ip1 = q(j, i + 1);
+                q(j, i) = c * q_i + s * q_ip1;
+                q(j, i + 1) = -s * q_i + c * q_ip1;
             }
-            r(i+1,i) = 0;
+            r(i + 1, i) = 0;
         }
     }
 
@@ -64,45 +60,42 @@ constexpr QRMatrix<T, R> qr_hessenberg( const Matrix<T,R,C> a )
 
 // Modified Gram-Schmidt QR Decomposition (Kept as alternative or for reference)
 // Actually, let's replace it with a more stable Householder or Givens for all matrices.
-template<typename T, Size R, Size C>
-constexpr QRMatrix<T, R> qr( const Matrix<T,R,C> a )
-{
-    static_assert( R==C, "QR decomposition must be a square matrix");
+template <typename T, Size R, Size C>
+constexpr QRMatrix<T, R> qr(const Matrix<T, R, C> a) {
+    static_assert(R == C, "QR decomposition must be a square matrix");
 
-    Matrix<T,R,R> q = eye<T,R>();
-    Matrix<T,R,R> r = a;
-    
-    for(Size j = 0; j < C; ++j)
-    {
-        for(Size i = R - 1; i > j; --i)
-        {
+    Matrix<T, R, R> q = eye<T, R>();
+    Matrix<T, R, R> r = a;
+
+    for (Size j = 0; j < C; ++j) {
+        for (Size i = R - 1; i > j; --i) {
             // Use Givens rotation to zero a(i, j) using a(i-1, j)
-            T x = r(i-1,j);
-            T y = r(i,j);
-            
+            T x = r(i - 1, j);
+            T y = r(i, j);
+
             if (consteig::abs(y) > static_cast<T>(1e-15)) {
-                T mag = consteig::sqrt(x*x + y*y);
+                T mag = consteig::sqrt(x * x + y * y);
                 if (mag > static_cast<T>(1e-18)) {
                     T c = x / mag;
                     T s = y / mag;
-                    
+
                     // R = G * R
                     // Only need to update columns from j to C-1
                     for (Size k = j; k < C; ++k) {
-                        T r_im1_k = r(i-1,k);
-                        T r_i_k = r(i,k);
-                        r(i-1,k) = c * r_im1_k + s * r_i_k;
-                        r(i,k) = -s * r_im1_k + c * r_i_k;
+                        T r_im1_k = r(i - 1, k);
+                        T r_i_k = r(i, k);
+                        r(i - 1, k) = c * r_im1_k + s * r_i_k;
+                        r(i, k) = -s * r_im1_k + c * r_i_k;
                     }
-                    
+
                     // Q = Q * G^T
                     for (Size k = 0; k < R; ++k) {
-                        T q_k_im1 = q(k,i-1);
-                        T q_k_i = q(k,i);
-                        q(k,i-1) = c * q_k_im1 + s * q_k_i;
-                        q(k,i) = -s * q_k_im1 + c * q_k_i;
+                        T q_k_im1 = q(k, i - 1);
+                        T q_k_i = q(k, i);
+                        q(k, i - 1) = c * q_k_im1 + s * q_k_i;
+                        q(k, i) = -s * q_k_im1 + c * q_k_i;
                     }
-                    r(i,j) = 0;
+                    r(i, j) = 0;
                 }
             }
         }
@@ -114,5 +107,5 @@ constexpr QRMatrix<T, R> qr( const Matrix<T,R,C> a )
     return res;
 }
 
-} //end namespace
+}  // namespace consteig
 #endif
