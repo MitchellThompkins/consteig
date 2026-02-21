ed7bcb4c034245b428488dca4de143b2ec56124b
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2026-02-21 16:52:59 +0000

    playing with extending pow to allow for exp

---
+++ math/functions/exp.hpp
@@ -0,0 +1,64 @@
+#ifndef CONSTMATH_EXP_HPP
+#define CONSTMATH_EXP_HPP
+
+//#include "../../consteig_options.hpp"
+//#include "utilities.hpp"  // For epsilon
+
+namespace consteig {
+
+namespace internal {
+template <typename T>
+constexpr T sqrt_recur(const T x, const T xn, const int count) {
+    return (abs(xn - x / xn) / (T(1) + xn) < epsilon<T>() ? xn
+            : count < CONSTEIG_MAX_ITER                   ? sqrt_recur(x, T(0.5) * (xn + x / xn), count + 1)
+                                                          : xn);
+}
+
+template <typename T>
+constexpr T sqrt_check(const T x, const T m_val) {
+    return (x == T(0)                      ? T(0)
+            : epsilon<T>() > abs(T(1) - x) ? x
+            : x > T(4)                     ? sqrt_check(x / T(4), T(2) * m_val)
+            : x < T(0.25)                  ? sqrt_check(x * T(4), m_val / T(2))
+                                           : m_val * sqrt_recur(x, x / T(2), 0));
+}
+
+template <typename T>
+constexpr T sqrt_int(const T x) {
+    // The closed guess will be stored in the root
+    T root{static_cast<T>(0)};
+
+    // Base cases
+    if (x == 0 || x == 1) {
+        root = x;
+    } else {
+        // Staring from 1, try all numbers until
+        // i*i is greater than or equal to val.
+        T i = 1, result = 1;
+        while (result <= x) {
+            i++;
+            result = i * i;
+        }
+        root = i - 1;
+    }
+
+    return root;
+}
+
+}  // namespace internal
+
+template <typename T>
+constexpr T sqrt(const T x) {
+    // TODO(mthompkins): Need to return NaN for negative numbers. This
+    // implementation is really really ugly
+    if (x < static_cast<T>(0))
+        return static_cast<T>(-1);
+    else if (is_float<T>())
+        return internal::sqrt_check(x, static_cast<T>(1));
+    else
+        return internal::sqrt_int(x);
+}
+
+}  // namespace consteig
+
+#endif
