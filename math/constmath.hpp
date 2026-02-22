752ca063bd58932d85a03517af2e4ec8eb293a02
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2026-02-22 07:14:00 +0000

    first pass

--- math/constmath.hpp
+++ math/constmath.hpp
@@ -3,9 +3,12 @@
 
 #include "complex.hpp"
 #include "functions/abs.hpp"
+#include "functions/exp.hpp"
 #include "functions/pow.hpp"
 #include "functions/sgn.hpp"
 #include "functions/sqrt.hpp"
 #include "functions/utilities.hpp"
+
+static constexpr doulbe e {2.71828182845904523536028747135266249775724709369995};
 
 #endif
