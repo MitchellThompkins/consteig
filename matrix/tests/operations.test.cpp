47287546b48d24f4e5ccc96627a8bab71c7f4a93
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2026-03-13 21:36:38 -0700

    tighten up tolerance

--- matrix/tests/operations.test.cpp
+++ matrix/tests/operations.test.cpp
@@ -5,8 +5,6 @@
 
 using namespace consteig;
 
-static constexpr float kThresh{CONSTEIG_FLOAT_TEST_TOLERANCE};
-
 TEST(matrix, eigen_comparison)
 {
     // Setup matrices
@@ -22,37 +20,37 @@ TEST(matrix, eigen_comparison)
     Eigen::Matrix3d eigSum = eigMat1 + eigMat2;
     for (int i = 0; i < 3; ++i)
         for (int j = 0; j < 3; ++j)
-            EXPECT_NEAR(sum(i, j), eigSum(i, j), kThresh);
+            EXPECT_NEAR(sum(i, j), eigSum(i, j), CONSTEIG_TEST_TOLERANCE);
 
     // Subtraction
     Matrix<double, 3, 3> diff = mat1 - mat2;
     Eigen::Matrix3d eigDiff = eigMat1 - eigMat2;
     for (int i = 0; i < 3; ++i)
         for (int j = 0; j < 3; ++j)
-            EXPECT_NEAR(diff(i, j), eigDiff(i, j), kThresh);
+            EXPECT_NEAR(diff(i, j), eigDiff(i, j), CONSTEIG_TEST_TOLERANCE);
 
     // Multiplication
     Matrix<double, 3, 3> prod = mat1 * mat2;
     Eigen::Matrix3d eigProd = eigMat1 * eigMat2;
     for (int i = 0; i < 3; ++i)
         for (int j = 0; j < 3; ++j)
-            EXPECT_NEAR(prod(i, j), eigProd(i, j), kThresh);
+            EXPECT_NEAR(prod(i, j), eigProd(i, j), CONSTEIG_TEST_TOLERANCE);
 
     // Transpose
     Matrix<double, 3, 3> trans = transpose(mat1);
     Eigen::Matrix3d eigTrans = eigMat1.transpose();
     for (int i = 0; i < 3; ++i)
         for (int j = 0; j < 3; ++j)
-            EXPECT_NEAR(trans(i, j), eigTrans(i, j), kThresh);
+            EXPECT_NEAR(trans(i, j), eigTrans(i, j), CONSTEIG_TEST_TOLERANCE);
 
     // Determinant
     double d = det(mat1); // 0 for this specific matrix (singular)
     double eigDet = eigMat1.determinant();
-    EXPECT_NEAR(d, eigDet, kThresh);
+    EXPECT_NEAR(d, eigDet, CONSTEIG_TEST_TOLERANCE);
 
     Matrix<double, 2, 2> mat3 = {{{{1, 2}, {3, 4}}}};
     Eigen::Matrix2d eigMat3 = toEigen(mat3);
-    EXPECT_NEAR(det(mat3), eigMat3.determinant(), kThresh);
+    EXPECT_NEAR(det(mat3), eigMat3.determinant(), CONSTEIG_TEST_TOLERANCE);
 }
 
 TEST(matrix, static_constexpr_addition)
@@ -208,11 +206,11 @@ TEST(matrix, static_constexpr_norm_euclidean)
     static constexpr float n2{normE(mat2)};
 
     static constexpr float answer1 = 8.0F;
-    static constexpr float answer2 = 7.9373F;
+    static constexpr float answer2 = 7.937253933F;
 
     // ASSERT_FLOAT_EQ will fail in this case, as answer2 above was rounded
-    ASSERT_NEAR(n1, answer1, kThresh);
-    ASSERT_NEAR(n2, answer2, kThresh);
+    ASSERT_NEAR(n1, answer1, CONSTEIG_FLOAT_TEST_TOLERANCE);
+    ASSERT_NEAR(n2, answer2, CONSTEIG_FLOAT_TEST_TOLERANCE);
 }
 
 TEST(matrix, static_constexpr_det)
@@ -235,6 +233,6 @@ TEST(matrix, static_constexpr_det)
     static_assert(n2 == answer2, MSG);
 
     // ASSER_FLOAT_EQ will fail in this case, as answer2 above was rounded
-    ASSERT_NEAR(n1, answer1, kThresh);
-    ASSERT_NEAR(n2, answer2, kThresh);
+    ASSERT_NEAR(n1, answer1, CONSTEIG_FLOAT_TEST_TOLERANCE);
+    ASSERT_NEAR(n2, answer2, CONSTEIG_FLOAT_TEST_TOLERANCE);
 }
