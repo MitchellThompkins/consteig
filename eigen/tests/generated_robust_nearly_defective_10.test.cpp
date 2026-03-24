98aa687377f3e91dcfe7394d862ec5e1b402e32e
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2026-03-24 04:20:03 +0000

    add these back in

---
+++ eigen/tests/generated_robust_nearly_defective_10.test.cpp
@@ -0,0 +1,7 @@
+#include "generated_test_helpers.hpp"
+TEST(generated_tests, nearly_defective_8x8_10)
+{
+    static_assert(check_single_nearly_defective_nonsym_8x8<10>(),
+                  "Test nearly_defective_8x8_10 failed");
+    SUCCEED();
+}
