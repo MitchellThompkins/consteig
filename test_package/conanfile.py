ea5b9d4d6f933faaedae0cfed5852d89094ef18c
Mitchell Thompkins <mitchell.thompkins@pm.me>
2026-03-30 03:20:41 +0000

    add test_package dir as per https://github.com/conan-io/conan-center-index/blob/master/docs/adding_packages/folders_and_files.md#test_package

---
+++ test_package/conanfile.py
@@ -0,0 +1,21 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout
+
+
+class TestPackage(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    def requirements(self):
+        self.requires(self.tested_reference)
+
+    def layout(self):
+        cmake_layout(self)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        self.run("./test_package")
