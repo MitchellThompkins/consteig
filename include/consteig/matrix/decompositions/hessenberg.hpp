b45ea1e7013e4bbcd9d62365dd639756a3edad74
Mitchell Thompkins <mitchell.thompkins@pm.me>
2026-06-28 05:42:10 +0000

    remove default constructors anyway if we are supporting aggregate init

--- include/consteig/matrix/decompositions/hessenberg.hpp
+++ include/consteig/matrix/decompositions/hessenberg.hpp
@@ -28,12 +28,6 @@ template <typename T, Size S> struct PHMatrix
 {
     Matrix<T, S, S> _p;
     Matrix<T, S, S> _h;
-
-    constexpr PHMatrix() = default;
-    constexpr PHMatrix(const PHMatrix &) = default;
-    constexpr PHMatrix(PHMatrix &&) = default;
-    constexpr PHMatrix &operator=(const PHMatrix &) = default;
-    constexpr PHMatrix &operator=(PHMatrix &&) = default;
 };
 
 /// @brief Reduce a square matrix to upper Hessenberg form.
